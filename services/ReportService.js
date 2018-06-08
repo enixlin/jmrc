1d114bf31ecbb39d7127145409b89ade50276803
enixlin <enixlin@126.com>
2018-06-08 17:55:06 +0800

    add report class

--- services/ReportService.js
+++ services/ReportService.js
@@ -1,18 +1,27 @@
 /**
  * 报表业务类
  * 
+ * 偿试使用组合模式来设计这个报表业务类
+ * 
+ * 
  */
 
 
 var db = require('./DataBaseService').localDB();
 
 
-var report = {
-
-
-
-
+class Report {
+    constructor(name) {
+        this.name = name;
+        this.children = [];
+    }
 }
 
+Report.prototype = {
+    //生成报表
+    create: function() { throw new Error("抽象类的方法不能调用,需在实现类中重写"); },
+    //显示报表 
+    show: function() { throw new Error("抽象类的方法不能调用,需在实现类中重写"); },
+};
 
 module.exports = report;
